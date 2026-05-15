3cf4b631652ce7bbbe22f9117fc8ae722a44b21b
MostafaGamalBisher <mostafagamalbisher@outlook.com>
2026-05-15 23:41:18 +0300

    feat: implement theme switcher and update Tailwind CSS configuration for dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Logo from './components/Logo';
+import ThemeSwitcher from './components/ThemeSwitcher';
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600 underline font-inter">
-        Tailwind is Working!
-      </h1>
+    <div className="font-inter">
+      <Logo />
+      <ThemeSwitcher />
     </div>
   );
 }
