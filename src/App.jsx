f1471605b222f4315a6c577bd1dc313b185fe399
MostafaGamalBisher <mostafagamalbisher@outlook.com>
2026-05-15 20:38:50 +0300

    feat: add new components and integrate Inter font in Tailwind CSS configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600 underline">
+      <h1 className="text-4xl font-bold text-blue-600 underline font-inter">
         Tailwind is Working!
       </h1>
     </div>
